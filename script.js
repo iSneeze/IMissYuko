55ef935bf69f2fc3b127992021190995c09159aa
iSneeze <justopenanissue@github>
2023-03-21 00:00:55 +0100

    fixed time display, added funny surprise, added beedo

--- script.js
+++ script.js
@@ -15,18 +15,17 @@ currentStreamInfo.then((response) => {
         let lastArchiveInfo = fetchAsync("https://holodex.net/api/v2/videos?channel_id=UCcIcMRVpNqWQn4WnDhRASAw&limit=1&status=past");
         lastArchiveInfo.then((archiveResponse) => {
 
-            let dateUTC = new Date().toUTCString();
-            let d = new Date(dateUTC);
+            let d = new Date();
             let delta = new Date(archiveResponse[0].available_at);
             timeSince = (d - delta) - archiveResponse[0].duration * 1000;
             let timer = new Date(timeSince);
-            document.getElementById("timer").innerHTML = "Last seen: " + Math.floor(Math.abs(timeSince / (24*3600*1000))) + "d " + timer.getHours() + "h " + timer.getMinutes() + "m " + timer.getSeconds() + "s ago streaming:";
+            document.getElementById("timer").innerHTML = "Last seen: " + Math.floor(Math.abs(timeSince / (24 * 3600 * 1000))) + "d " + timer.getUTCHours() + "h " + timer.getMinutes() + "m " + timer.getSeconds() + "s ago streaming:";
             document.getElementById("thumbnail").innerHTML = '<iframe src="https://www.youtube.com/embed/' + archiveResponse[0].id + '" title="YouTube video player" frameborder="0" allow="clipboard-write; web-share" allowfullscreen></iframe>';
 
             window.setInterval(function () {
                 timeSince = timeSince + 1000
                 let timer = new Date(timeSince);
-                document.getElementById("timer").innerHTML = "Last seen: " + Math.floor(Math.abs(timeSince / (24*3600*1000))) + "d " + timer.getHours() + "h " + timer.getMinutes() + "m " + timer.getSeconds() + "s ago streaming:";
+                document.getElementById("timer").innerHTML = "Last seen: " + Math.floor(Math.abs(timeSince / (24 * 3600 * 1000))) + "d " + timer.getUTCHours() + "h " + timer.getMinutes() + "m " + timer.getSeconds() + "s ago streaming:";
             }, 1000)
         })
     } else {
@@ -36,21 +35,20 @@ currentStreamInfo.then((response) => {
             document.getElementById("thumbnail").innerHTML = '<iframe src="https://www.youtube.com/embed/' + response[0].id + '" title="YouTube video player" frameborder="0" allow="clipboard-write; web-share" allowfullscreen></iframe>';
 
         } else {
-            let dateUTC = new Date().toUTCString();
-            let d = new Date(dateUTC);
+            let d = new Date();
             let delta = new Date(response[0].start_scheduled);
             timeUntil = delta - d
             let timer = new Date(timeUntil);
-            document.getElementById("timer").innerHTML = "Time until next stream: " + Math.floor(Math.abs(timeUntil / (24*3600*1000))) + "d " + timer.getHours() + "h " + timer.getMinutes() + "m " + timer.getSeconds() + "s";
+            document.getElementById("timer").innerHTML = "Time until next stream: " + Math.floor(Math.abs(timeUntil / (24 * 3600 * 1000))) + "d " + timer.getUTCHours() + "h " + timer.getMinutes() + "m " + timer.getSeconds() + "s";
             document.getElementById("thumbnail").innerHTML = '<iframe src="https://www.youtube.com/embed/' + response[0].id + '" title="YouTube video player" frameborder="0" allow="clipboard-write; web-share" allowfullscreen></iframe>';
 
             window.setInterval(function () {
                 timeUntil = timeUntil - 1000
                 let timer = new Date(timeUntil);
-                if(timeUntil > 0){
-                    document.getElementById("timer").innerHTML = "Time until next stream: " + Math.floor(Math.abs(timeUntil / (24*3600*1000))) + "d " + timer.getHours() + "h " + timer.getMinutes() + "m " + timer.getSeconds() + "s";
+                if (timeUntil > 0) {
+                    document.getElementById("timer").innerHTML = "Time until next stream: " + Math.floor(Math.abs(timeUntil / (24 * 3600 * 1000))) + "d " + timer.getUTCHours() + "h " + timer.getMinutes() + "m " + timer.getSeconds() + "s";
                 } else {
-                    document.getElementById("timer").innerHTML = "Stream about to start!"; 
+                    document.getElementById("timer").innerHTML = "Stream about to start!";
                 }
             }, 1000)
         }
@@ -59,73 +57,91 @@ currentStreamInfo.then((response) => {
 })
 
 function getRandomFileName() {
-    let files = ['beedo', 'brap', 'coom1', 'coom2', 'correction', 'crash1', 'cringe', 'dontmissmetoomuch', 'faq', 'hic1',
+    let files = ['beedo', 'beedobeedo', 'brap', 'coom1', 'coom2', 'correction', 'crash1', 'cringe', 'dontmissmetoomuch', 'faq', 'hic1',
         'hic2', 'hic3', 'laugh1', 'laughsnort', 'memberwisp', 'nocrash', 'norubber', 'notafurry1', 'pee1', 'peepeepoopoo',
         'pickle2', 'picklefits', 'pink', 'putonthehoodie', 'rizz', 'spit', 'thatscrazy', 'touchtoes', 'wanau1', 'wanau2',
         'wanau3', 'wanau4', 'wanau5', 'wanau6', 'yukonade', 'yukosplain', 'whyamIlikethis'];
     return files[Math.floor(Math.random() * files.length)]
 }
 
-var audio = new Audio("");
+function spinTheWheel() {
+    if (Math.floor(Math.random() * 100) == 0) { // 1% chance
+        audio.src = '/assets/sound/rick.m4a';
+        lucky = true;
+    }
+    
+}
 
-document.getElementById("btn").onclick = function() {
+function display(filename, timeMS) {
+    document.getElementById("imgpopup").src = '/assets/images/' + filename + '.png';
+    document.getElementById("imgpopup").style.display = "block";
+    setTimeout(() => {
+        document.getElementById("imgpopup").style.display = "none";
+    }, timeMS);
+}
+
+var audio = new Audio("");
+var lucky = false;
+document.getElementById("btn").onclick = function () {
     let file = getRandomFileName();
     audio.src = '/assets/sound/' + file + '.m4a';
+
+    if (!lucky) { //😏
+        spinTheWheel();
+    }
+
     audio.load();
-    audio.play()
+    audio.play();
 
 
     if (audio.src.includes("wanau")) {
-    popEmote("wanau");
-        document.getElementById("wanau").style.display = "block";
-        setTimeout(() => {
-            document.getElementById("wanau").style.display = "none";
-        }, 800);
+        popEmote("wanau");
+        display("wanau", 800);
     }
 
     if (audio.src.includes("hic")) {
-    popEmote("hic");
-        document.getElementById("hic").style.display = "block";
-        setTimeout(() => {
-            document.getElementById("hic").style.display = "none";
-        }, 500);
+        popEmote("hic");
+        display("hic", 500);
+    }
+
+    if (audio.src.includes("rick.m4a")) {
+        popEmote("rick");
+        display("rick", 2000);
     }
 
     if (audio.src.includes("faq") || audio.src.includes("rizz")) {
-    popEmote("yuyuCry");
-        document.getElementById("sob").style.display = "block";
-        setTimeout(() => {
-            document.getElementById("sob").style.display = "none";
-        }, 1000);
+        popEmote("yuyuCry");
+        display("sob", 1000);
     }
 }
+
 
 //Accepts a string parameter for loading different images
 //lifetime optional parameter doesn't change styling options
 function popEmote(imgName, numElements = 3, lifetime = 1000) {
-  const imgSource = "./assets/images/" + imgName + ".png";
-  //Measured in vh and vw
-  const topBound = 5;
-  const bottomBound = 90;
-  const leftBound = 10;
-  const rightBound = 90;
+    const imgSource = "./assets/images/" + imgName + ".png";
+    //Measured in vh and vw
+    const topBound = 5;
+    const bottomBound = 90;
+    const leftBound = 10;
+    const rightBound = 90;
 
-  for (let i = 0; i < numElements; i++) {
-    const imgElement = document.createElement("img");
-    imgElement.src = imgSource;
+    for (let i = 0; i < numElements; i++) {
+        const imgElement = document.createElement("img");
+        imgElement.src = imgSource;
 
-    const popDiv = document.createElement("div");
-    popDiv.appendChild(imgElement);
-    popDiv.classList.add("emote-pop");
+        const popDiv = document.createElement("div");
+        popDiv.appendChild(imgElement);
+        popDiv.classList.add("emote-pop");
 
-    popDiv.style.top = Math.random() * (bottomBound - topBound) + "vh";
-    popDiv.style.left = Math.random() * (rightBound - leftBound) + "vw";
+        popDiv.style.top = Math.random() * (bottomBound - topBound) + "vh";
+        popDiv.style.left = Math.random() * (rightBound - leftBound) + "vw";
 
-    let popupLayer = document.getElementById("popup-layer");
-    popupLayer.appendChild(popDiv);
+        let popupLayer = document.getElementById("popup-layer");
+        popupLayer.appendChild(popDiv);
 
-    setTimeout(() => {
-      popDiv.remove();
-    }, lifetime);
-  }
+        setTimeout(() => {
+            popDiv.remove();
+        }, lifetime);
+    }
 }
